0a0537a3aab31acad4624fc1ac4efd065d9da9af
Deifa <Deifa@users.noreply.github.com>
2020-12-23 20:52:37 +0100

    Responsive Fixes

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 const navbarToggler = document.getElementsByClassName('navbar-toggler')[0];
+const navbarToggler1 = document.getElementsByClassName('navbar-toggler')[1];
 const navlinksDiv = document.getElementsByClassName('navlinks-div')[0];
 
-navbarToggler.addEventListener('click', () => {
+const activeToggler = () => {
     navlinksDiv.classList.toggle('active');
-})
+}
+
+navbarToggler.addEventListener('click', activeToggler)
+
+navbarToggler1.addEventListener('click', activeToggler)
